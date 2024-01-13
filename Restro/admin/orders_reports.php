8b4e487d814a0476937560502b4992497c2f307d
Sangnguyen79776 <sangntFX20325@funix.edu.vn>
2024-01-13 11:57:53 -0600

    files

--- Restro/admin/orders_reports.php
+++ Restro/admin/orders_reports.php
@@ -55,7 +55,7 @@ require_once('partials/_head.php');
                                     $stmt->execute();
                                     $res = $stmt->get_result();
                                     while ($order = $res->fetch_object()) {
-                                        $total = ($order->prod_price * $order->prod_qty);
+                                        $total = ((int)$order->prod_price * (int)$order->prod_qty);
 
                                     ?>
                                         <tr>
